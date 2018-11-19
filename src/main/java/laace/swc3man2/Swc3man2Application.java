b7503236798883eb70da59c1ccad74e4f9ce973d
Lime Icing <emil577b@stud.kea.dk>
2018-11-19 15:43:46 +0100

    initialized Spring project

---
+++ src/main/java/laace/swc3man2/Swc3man2Application.java
@@ -0,0 +1,12 @@
+package laace.swc3man2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Swc3man2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Swc3man2Application.class, args);
+    }
+}
