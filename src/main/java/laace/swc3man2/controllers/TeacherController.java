e7f456b7fafa95a3f4f10993de8cabf664f33266
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-11-24 10:55:40 +0100

    moved index out of static, suppost to be in template folder. Made two methods in TeacherRepositioryINterface, a getALL and a GetID. Made a small HTML page to display teachers and set up the TeacherController to display the teachers

--- src/main/java/laace/swc3man2/controllers/TeacherController.java
+++ src/main/java/laace/swc3man2/controllers/TeacherController.java
@@ -1,4 +1,21 @@
 package laace.swc3man2.controllers;
 
+import laace.swc3man2.interfaces.TeacherRepositoryInterface;
+import laace.swc3man2.models.TeacherModel;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class TeacherController {
+
+    @Autowired
+    TeacherRepositoryInterface TRI;
+
+    @GetMapping("/teachers")
+    public String teachersPage(Model model) {
+        model.addAttribute("teachers", TRI.findAll() );
+        return "teachers";
+    }
 }
--- src/main/java/laace/swc3man2/interfaces/TeacherRepositoryInterface.java
+++ src/main/java/laace/swc3man2/interfaces/TeacherRepositoryInterface.java
@@ -4,7 +4,13 @@ import laace.swc3man2.models.TeacherModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+
 @Repository
 public interface TeacherRepositoryInterface extends JpaRepository<TeacherModel, Integer> {
+    @Override
+    List<TeacherModel> findAll();
 
+    @Override
+    TeacherModel getOne(Integer integer);
 }
