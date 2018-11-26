88c34fa02eb2d099528d2cce6cd4ef2382871ba7
Lime Icing <emil577b@stud.kea.dk>
2018-11-26 15:24:56 +0100

    fixed merge conflict

--- src/main/java/laace/swc3man2/Swc3man2Application.java
+++ src/main/java/laace/swc3man2/Swc3man2Application.java
@@ -1,10 +1,7 @@
 package laace.swc3man2;
 
-<<<<<<< HEAD
 import laace.swc3man2.services.CourseService;
 import org.springframework.beans.factory.annotation.Autowired;
-=======
->>>>>>> 5df3010ca756767ef08a3d21925e01e230c6584c
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
@@ -12,21 +9,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class Swc3man2Application implements CommandLineRunner {
 
-<<<<<<< HEAD
     @Autowired
     private CourseService courseService;
 
-=======
->>>>>>> 5df3010ca756767ef08a3d21925e01e230c6584c
     public static void main(String[] args) {
         SpringApplication.run(Swc3man2Application.class, args);
     }
 
     @Override
     public void run(String... args) throws Exception {
-<<<<<<< HEAD
         courseService.fetchFromAPI();
-=======
->>>>>>> 5df3010ca756767ef08a3d21925e01e230c6584c
     }
 }
