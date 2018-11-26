5893d74b907765a8bcc741cca126799b5eb82bcf
Lime Icing <emil577b@stud.kea.dk>
2018-11-26 18:50:43 +0100

    united CourseRepositories

--- src/main/java/laace/swc3man2/repositories/CourseRepository.java
+++ src/main/java/laace/swc3man2/repositories/CourseRepository.java
@@ -3,8 +3,11 @@ package laace.swc3man2.repositories;
 import laace.swc3man2.models.CourseModel;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
+import java.util.List;
 
 @Repository
 public interface CourseRepository extends JpaRepository<CourseModel, Integer> {
 
+    @Override
+    List<CourseModel> findAll();
 }
