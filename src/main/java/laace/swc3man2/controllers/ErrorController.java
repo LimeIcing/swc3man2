3155b2cbb2f72b116ab6942a575eaece66973f15
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-12-05 14:14:57 +0100

    made a better error page. Top Notch

--- src/main/java/laace/swc3man2/controllers/ErrorController.java
+++ src/main/java/laace/swc3man2/controllers/ErrorController.java
@@ -1,38 +1,31 @@
 package laace.swc3man2.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
 
 import javax.servlet.RequestDispatcher;
 import javax.servlet.http.HttpServletRequest;
 
-@Controller
-public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+@ControllerAdvice
+public class ErrorController {
+
+    private static Logger logger = LoggerFactory.getLogger(ErrorController.class);
 
 
-    @RequestMapping("/error")
-    public String handleError(HttpServletRequest request) {
-        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
-
-        if (status != null) {
-            Integer statusCode = Integer.valueOf(status.toString());
-
-            if(statusCode == HttpStatus.BAD_REQUEST.value()) {
-                return "error-404";
-            }
-            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
-                System.out.println(getErrorPath());
-                return "error-500";
-            }
-
-        }
+    @ExceptionHandler(Throwable.class)
+    @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
+    public String exception(final Throwable throwable, final Model model) {
+        logger.error("Exception during execution of SpringSecurity application", throwable);
+        String errorMessage = (throwable != null ? throwable.getMessage() : "Unknown error");
+        model.addAttribute("errorMessage", errorMessage);
         return "error";
     }
-
-        @Override
-        public String getErrorPath() {
-            return "/error";
-        }
-
+    
 }
