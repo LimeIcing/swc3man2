b1f2720c12576b297ccda448c5bf8c47228a475f
knoldesparker <5735792+knoldesparker@users.noreply.github.com>
2018-12-05 14:02:16 +0100

    A Basic Error page. No Printing Error Code

---
+++ src/main/java/laace/swc3man2/controllers/ErrorController.java
@@ -0,0 +1,38 @@
+package laace.swc3man2.controllers;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.servlet.RequestDispatcher;
+import javax.servlet.http.HttpServletRequest;
+
+@Controller
+public class ErrorController implements org.springframework.boot.web.servlet.error.ErrorController {
+
+
+    @RequestMapping("/error")
+    public String handleError(HttpServletRequest request) {
+        Object status = request.getAttribute(RequestDispatcher.ERROR_STATUS_CODE);
+
+        if (status != null) {
+            Integer statusCode = Integer.valueOf(status.toString());
+
+            if(statusCode == HttpStatus.BAD_REQUEST.value()) {
+                return "error-404";
+            }
+            else if(statusCode == HttpStatus.INTERNAL_SERVER_ERROR.value()) {
+                System.out.println(getErrorPath());
+                return "error-500";
+            }
+
+        }
+        return "error";
+    }
+
+        @Override
+        public String getErrorPath() {
+            return "/error";
+        }
+
+}
