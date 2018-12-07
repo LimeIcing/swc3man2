81ed24e306585d72eebf697465633e266b2a2513
Lime Icing <emil577b@stud.kea.dk>
2018-12-07 12:20:11 +0100

    course creation to API still not working, but I'm catching the exception now

--- src/main/java/laace/swc3man2/services/CourseService.java
+++ src/main/java/laace/swc3man2/services/CourseService.java
@@ -3,14 +3,16 @@ package laace.swc3man2.services;
 import com.fasterxml.jackson.core.type.TypeReference;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import laace.swc3man2.models.CourseModel;
+import laace.swc3man2.models.CourseModelAPI;
 import laace.swc3man2.repositories.CourseRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
-import org.springframework.http.HttpEntity;
-import org.springframework.http.HttpMethod;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.*;
 import org.springframework.stereotype.Service;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
 
 import java.io.IOException;
@@ -45,29 +47,56 @@ public class CourseService {
         }
     }
 
-    public void postCourseToAPI(CourseModel courseModel) {
-        HttpEntity<CourseModel> request = new HttpEntity<>(courseModel);
-        ResponseEntity<CourseModel> response = restTemplate.exchange(
-                legacyURL, HttpMethod.POST, request, CourseModel.class);
-        CourseModel responseCourse = response.getBody();
+    /*
+    new CourseModelAPI(courseModel.getId(),
+                courseModel.getSemester(), courseModel.getEcts(), courseModel.getNumberOfTeachers(),
+                courseModel.getName(), courseModel.getStudyprogramme(), courseModel.getNamedanish(),
+                courseModel.getDescription(), courseModel.getLanguange(), courseModel.isMandatory())
+     */
 
-        if (responseCourse.getId() == courseModel.getId()){
-            System.out.println("Posted successfully to API");
-        } else {
-            System.out.println("Post to API failed");
+    public void postCourseToAPI(CourseModel courseModel) {
+        HttpHeaders headers = new HttpHeaders();
+        MultiValueMap<String, String> map = new LinkedMultiValueMap<>();
+        HttpEntity<MultiValueMap<String, String>> request;
+        ResponseEntity<String> response;
+
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        map.add("id", "100");
+        request = new HttpEntity<>(map, headers);
+
+        try {
+            response = restTemplate.postForEntity(legacyURL, request, String.class);
+            System.out.println(response.getStatusCode());
+        } catch (HttpClientErrorException hCEE) {
+            hCEE.printStackTrace();
         }
     }
 
     /*
-ResponseEntity<Foo> response = restTemplate
-  .exchange(fooResourceUrl, HttpMethod.POST, request, Foo.class);
+Next, let’s look at how to submit a form using the POST method.
+
+First, we need to set the “Content-Type” header to application/x-www-form-urlencoded.
+
+This makes sure that a large query string can be sent to the server, containing name/value pairs separated by ‘&‘:
+
+HttpHeaders headers = new HttpHeaders();
+headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
+
+We can wrap the form variables into a LinkedMultiValueMap:
+
+MultiValueMap<String, String> map= new LinkedMultiValueMap<>();
+map.add("id", "1");
+
+Next, we build the Request using an HttpEntity instance:
+
+HttpEntity<MultiValueMap<String, String>> request = new HttpEntity<>(map, headers);
+
+Finally, we can connect to the REST service by calling restTemplate.postForEntity() on the Endpoint: /foos/form
+
+ResponseEntity<String> response = restTemplate.postForEntity(
+  fooResourceUrl+"/form", request , String.class);
 
 assertThat(response.getStatusCode(), is(HttpStatus.CREATED));
-
-Foo foo = response.getBody();
-
-assertThat(foo, notNullValue());
-assertThat(foo.getName(), is("bar"));
      */
 
     public Page<CourseModel> listAll(int page) {
